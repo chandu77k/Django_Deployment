ad5a4231fbca663b24bda4d6697356cf72e43f3d
chandu77k <118392074+chandu77k@users.noreply.github.com>
2025-05-06 16:33:57 +0530

    Delete DjangoProject directory

--- DjangoProject/DjangoProject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from myapp import views  # Import the view from your app
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Empty path
-]
--- DjangoProject/myapp/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'index.html')  # Template located in templates/home.html
